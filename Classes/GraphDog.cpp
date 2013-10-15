1a08a3bd35c727f7af902d44b85a2cddebf283c5
litqoo <litqoo@gmail.com>
2013-10-15 16:11:23 +0900

    garphdog 서버 교체 (litqoo.com -> nhn ent server)

--- Classes/GraphDog.cpp
+++ Classes/GraphDog.cpp
@@ -309,8 +309,8 @@ void* GraphDog::t_function(void *_insertIndex)
 	string paramStr = toBase64(desEncryption(graphdog->sKey, command.commandStr));
 	string dataset = "&token=" + token + "&command=" + paramStr + "&appver=" + GraphDog::get()->getAppVersionString() + "&version="+GRAPHDOG_VERSION;
 	//CCLog("t_function3");
-	string commandurl = "http://litqoo.com/dgserver/data.php";
-//	string commandurl = "http://182.162.201.147:10010/data.php"; //"http://182.162.201.147:10010/data.php"; //
+	//string commandurl = "http://litqoo.com/dgserver/data.php";
+	string commandurl = "http://182.162.201.147:10010/data.php"; //"http://182.162.201.147:10010/data.php"; //
     //commandurl=commandurl.append(GraphDog::get()->getGraphDogVersion());
     //commandurl=commandurl.append("/");
     //commandurl=commandurl.append(GraphDog::get()->aID);
