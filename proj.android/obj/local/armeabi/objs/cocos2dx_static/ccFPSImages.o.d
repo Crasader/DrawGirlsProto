471d1283e965d33f8e495d325c944c7eefc7b2c7
hsnks100 <hsnks100@gmail.com>
2013-10-31 22:34:07 +0900

    안드로이드 로컬에서 생성되는 이미지들은 삭제처리.

--- proj.android/obj/local/armeabi/objs/cocos2dx_static/ccFPSImages.o.d
+++
@@ -1,5 +0,0 @@
-obj/local/armeabi/objs/cocos2dx_static/ccFPSImages.o: \
- /Users/sawon3/Desktop/cocos2d-x-2.1.5/projects/DGproto/proj.android/../../../cocos2dx/ccFPSImages.c \
- /Users/sawon3/Desktop/cocos2d-x-2.1.5/projects/DGproto/proj.android/../../../cocos2dx/ccFPSImages.h
-
-/Users/sawon3/Desktop/cocos2d-x-2.1.5/projects/DGproto/proj.android/../../../cocos2dx/ccFPSImages.h:
