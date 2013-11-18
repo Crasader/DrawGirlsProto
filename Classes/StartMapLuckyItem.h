f41e12f88e0a83bb1b8483c2829a3f8ee27f2c04
seo88youngho <seo88youngho@gmail.com>
2013-11-18 18:17:45 +0900

    행운 아이템 효과 적용 몬스터 hp바 디자인 적용

--- Classes/StartMapLuckyItem.h
+++ Classes/StartMapLuckyItem.h
@@ -11,10 +11,19 @@
 
 #include "cocos2d.h"
 #include "GameData.h"
+#include "cocos-ext.h"
 
 USING_NS_CC;
+USING_NS_CC_EXT;
 
-class StartMapLuckyItem : public CCSprite
+enum StartMapLuckyItemZorder
+{
+	kStartMapLuckyItem_Z_img = 1,
+	kStartMapLuckyItem_Z_particle,
+	kStartMapLuckyItem_Z_fx
+};
+
+class StartMapLuckyItem : public CCNode
 {
 public:
 	static StartMapLuckyItem* create(IntPoint t_point)
@@ -47,19 +56,72 @@ public:
 	
 private:
 	IntPoint my_point;
+	CCSprite* main_img;
 	
 	void startMyAction()
 	{
-		runAction(CCScaleTo::create(1.f, 0.7f));
+		main_img->runAction(CCScaleTo::create(1.f, 0.7f));
+	}
+	
+	void createParticle()
+	{
+		CCParticleSystemQuad* bomb_particle = CCParticleSystemQuad::createWithTotalParticles(150);
+		bomb_particle->setPositionType(kCCPositionTypeRelative);
+		bomb_particle->setTexture(CCTextureCache::sharedTextureCache()->addImage("particle2.png"));
+		bomb_particle->setEmissionRate(5000);
+		bomb_particle->setAngle(90.0);
+		bomb_particle->setAngleVar(360.0);
+		ccBlendFunc blendFunc = {GL_SRC_ALPHA, GL_ONE};
+		bomb_particle->setBlendFunc(blendFunc);
+		bomb_particle->setDuration(0.01f);
+		bomb_particle->setEmitterMode(kCCParticleModeGravity);
+		bomb_particle->setStartColor(ccc4f(0.f, 1.f, 1.f, 1.f));
+		bomb_particle->setStartColorVar(ccc4f(0,0.16f,0.16f,0.f));
+		bomb_particle->setEndColor(ccc4f(0.4f,0.8f,1.f,0.f));
+		bomb_particle->setEndColorVar(ccc4f(0, 0.16f, 0.16f, 0.f));
+		bomb_particle->setStartSize(0.f);
+		bomb_particle->setStartSizeVar(0.f);
+		bomb_particle->setEndSize(20.0);
+		bomb_particle->setEndSizeVar(5.0);
+		bomb_particle->setGravity(ccp(0.f,0.f));
+		bomb_particle->setRadialAccel(0.0);
+		bomb_particle->setRadialAccelVar(0.0);
+		bomb_particle->setSpeed(60);
+		bomb_particle->setSpeedVar(30.0);
+		bomb_particle->setTangentialAccel(0);
+		bomb_particle->setTangentialAccelVar(10);
+		bomb_particle->setTotalParticles(150);
+		bomb_particle->setLife(0.00);
+		bomb_particle->setLifeVar(1.2);
+		bomb_particle->setStartSpin(0.0);
+		bomb_particle->setStartSpinVar(0.f);
+		bomb_particle->setEndSpin(0.0);
+		bomb_particle->setEndSpinVar(0.f);
+		bomb_particle->setPosVar(ccp(0.f,0.f));
+		bomb_particle->setPosition(my_point.convertToCCP());
+		bomb_particle->setAutoRemoveOnFinish(true);
+		bomb_particle->setScale(0.7);
+		addChild(bomb_particle, kStartMapLuckyItem_Z_particle);
 	}
 	
 	void speedUpAction()
 	{
-		stopAllActions();
+		main_img->stopAllActions();
+		
+		CCNodeLoaderLibrary* nodeLoader = CCNodeLoaderLibrary::sharedCCNodeLoaderLibrary();
+		CCBReader* reader = new CCBReader(nodeLoader);
+		CCSprite* fx_lucky = dynamic_cast<CCSprite*>(reader->readNodeGraphFromFile("fx_lucky.ccbi",this));
+		fx_lucky->setPosition(my_point.convertToCCP());
+		fx_lucky->setScale(0.7);
+		addChild(fx_lucky, kStartMapLuckyItem_Z_fx);
+		
+		CCDelayTime* t_delay = CCDelayTime::create(0.7f);
+		CCCallFunc* t_call = CCCallFunc::create(this, callfunc_selector(StartMapLuckyItem::createParticle));
+		CCSequence* particle_seq = CCSequence::createWithTwoActions(t_delay, t_call);
 		
 		CCScaleTo* t_scale1 = CCScaleTo::create(0.5f, 1.f);
 		CCFadeTo* t_fade1 = CCFadeTo::create(0.5f, 195);
-		CCSpawn* t_spawn1 = CCSpawn::createWithTwoActions(t_scale1, t_fade1);
+		CCSpawn* t_spawn1 = CCSpawn::create(t_scale1, t_fade1, NULL);
 		CCCallFunc* t_call1 = CCCallFunc::create(this, callfunc_selector(StartMapLuckyItem::speedUp));
 		
 		CCScaleTo* t_scale2 = CCScaleTo::create(0.5f, 1.3f);
@@ -76,8 +138,11 @@ private:
 		CCFadeTo* t_fade4 = CCFadeTo::create(0.5f, 0);
 		CCSpawn* t_spawn4 = CCSpawn::createWithTwoActions(t_scale4, t_fade4);
 		CCCallFunc* t_call4 = CCCallFunc::create(this, callfunc_selector(StartMapLuckyItem::removeFromParent));
+		CCSequence* img_seq = CCSequence::create(t_spawn1, t_call1, t_spawn2, t_call2, t_spawn3, t_call3, t_spawn4, t_call4, NULL);
 		
-		runAction(CCSequence::create(t_spawn1, t_call1, t_spawn2, t_call2, t_spawn3, t_call3, t_spawn4, t_call4, NULL));
+		CCSpawn* total_spawn = CCSpawn::createWithTwoActions(particle_seq, img_seq);
+		
+		main_img->runAction(total_spawn);
 	}
 	
 	void speedUp()
@@ -87,19 +152,20 @@ private:
 	
 	void startRemoveAction()
 	{
-		stopAllActions();
-		runAction(CCSequence::createWithTwoActions(CCScaleTo::create(1.f, 0.f), CCCallFunc::create(this, callfunc_selector(StartMapLuckyItem::removeFromParent))));
+		main_img->stopAllActions();
+		main_img->runAction(CCSequence::createWithTwoActions(CCScaleTo::create(1.f, 0.f), CCCallFunc::create(this, callfunc_selector(StartMapLuckyItem::removeFromParent))));
 	}
 	
 	void myInit(IntPoint t_point)
 	{
 		my_point = IntPoint(t_point.x, t_point.y);
 		
-		initWithFile("item_lucky.png");
+		main_img = CCSprite::create("item_lucky.png");
+		main_img->setPosition(my_point.convertToCCP());
+		main_img->setScale(0.f);
+		addChild(main_img, kStartMapLuckyItem_Z_img);
 		
-		setScale(0.f);
-		
-		setPosition(my_point.convertToCCP());
+		setPosition(CCPointZero);
 		startMyAction();
 	}
 };
