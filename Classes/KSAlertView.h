f923ec4220fb02974f232b2c76d35669f2b4a77d
hsnks100 <hsnks100@gmail.com>
2013-11-25 21:34:50 +0900

    KSAlertView 주석 추가.

--- Classes/KSAlertView.h
+++ Classes/KSAlertView.h
@@ -77,27 +77,28 @@ public:
 	void scrollViewDidZoom(CCScrollView* view);
 	static KSAlertView* create();
 	bool init();
-	void addButton(CCMenuItemLambda* item);
-	void setCloseButton(CCMenuItemLambda* item);
-	void show();
+	void addButton(CCMenuItemLambda* item); // 버튼을 추가함
+	void setCloseButton(CCMenuItemLambda* item); // 우측 상단 닫기 버튼을 설정함
+	void show(); // 창을 띄움, 창을 띄우기 전에 모든게 세팅이 되어야 함.
 	
 	ScrollBar* m_scrollBar;
-	CC_SYNTHESIZE(CCNode*, m_contentNode, ContentNode);
-	CC_SYNTHESIZE(int, m_centerX, CenterX);
-	CC_SYNTHESIZE(int, m_centerY, CenterY);
-	CC_SYNTHESIZE(int, m_width, Width);
-	CC_SYNTHESIZE(int, m_height, Height);
-	CC_SYNTHESIZE(bool, m_shown, IsShown);
-//	CC_SYNTHESIZE(bool, m_existCloseButton, ExistCloseButton);
-	CC_SYNTHESIZE(bool, m_horizonScroll, HorizonScroll);
-	CC_SYNTHESIZE(bool, m_verticalScroll, VerticalScroll);
-//	CC_SYNTHESIZE(std::string, m_buttonFile, ButtonFile);
-	CC_SYNTHESIZE(std::string, m_titleStr, TitleStr);
-//	CC_SYNTHESIZE(std::string, m_closeButtonFile, CloseButtonFile);
+	CC_SYNTHESIZE(CCNode*, m_contentNode, ContentNode); // 내용물
+	CC_SYNTHESIZE(int, m_centerX, CenterX); // 팝업이 뜬 위치가 맘에 안들면 조절해야함. 기본값 가운데
+	CC_SYNTHESIZE(int, m_centerY, CenterY); // 팝업이 뜬 위치가 맘에 안들면 조절해야함. 기본값 가운데
+	CC_SYNTHESIZE(int, m_width, Width);   // 전체 프레임의 가로크기
+	CC_SYNTHESIZE(int, m_height, Height); // 전체 프레임의 세로 크기
+	CC_SYNTHESIZE(bool, m_shown, IsShown); // 알 필요 없음
+
+	CC_SYNTHESIZE(bool, m_horizonScroll, HorizonScroll);   // 알 필요없음
+	CC_SYNTHESIZE(bool, m_verticalScroll, VerticalScroll); // 알 필요없음
+	
+	CC_SYNTHESIZE(std::string, m_titleStr, TitleStr); // 타이틀 문자열, 있으면 위에서 여백이 생김
+
+	// 내용물 테두리, 이건 설정안해도 기본값 존재... 바꿔야할 듯.
 	CC_SYNTHESIZE(CCScale9Sprite*, m_contentBorder, ContentBorder);
-	CC_SYNTHESIZE(CCScale9Sprite*, m_hScroll, HScroll);
-	CC_SYNTHESIZE(CCScale9Sprite*, m_vScroll, VScroll);
-	CC_SYNTHESIZE(CCScale9Sprite*, m_back9, Back9);
-	CC_SYNTHESIZE(bool, m_closeOnPress, CloseOnPress);
+	CC_SYNTHESIZE(CCScale9Sprite*, m_hScroll, HScroll); // 가로 스크롤, 설정안하면 안보임
+	CC_SYNTHESIZE(CCScale9Sprite*, m_vScroll, VScroll); // 세로 스크롤, 설정안하면 안보임
+	CC_SYNTHESIZE(CCScale9Sprite*, m_back9, Back9);    // 배경이미지
+	CC_SYNTHESIZE(bool, m_closeOnPress, CloseOnPress); // 눌렀을 때 닫히게 할거냐, 기본값 true
 };
 
