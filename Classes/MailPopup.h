6140f6f9bdd4191bd975fb79ad127f4fa65c39b7
hsnks100 <hsnks100@gmail.com>
2014-01-23 18:17:29 +0900

    캐스팅 캔슬 성공 메시지. 텔포 버그 수정.

--- Classes/MailPopup.h
+++ Classes/MailPopup.h
@@ -17,32 +17,32 @@ using namespace std;
 #define LZZ_INLINE inline
 enum MailPopupZorder
 {
-  kMP_Z_gray = 1,
-  kMP_Z_back,
-  kMP_Z_close,
-  kMP_Z_content,
-  kMP_Z_mailTable,
-  kMP_Z_profileImg,
+	kMP_Z_gray = 1,
+	kMP_Z_back,
+	kMP_Z_close,
+	kMP_Z_content,
+	kMP_Z_mailTable,
+	kMP_Z_profileImg,
 	kMP_Z_helpAccept
 };
 enum MailTableViewTag
 {
-  kMP_MT_title = 1,
-  kMP_MT_score,
-  kMP_MT_rank,
-  kMP_MT_send,
-  kMP_MT_getheart,
-  kMP_MT_profileImg
+	kMP_MT_title = 1,
+	kMP_MT_score,
+	kMP_MT_rank,
+	kMP_MT_send,
+	kMP_MT_getheart,
+	kMP_MT_profileImg
 };
 enum MailPopupMenuTag
 {
-  kMP_MT_close = 1,
-  kMP_MT_invite,
-  kMP_MT_send_close,
-  kMP_MT_invite_close,
-  kMP_MT_invite_rank,
-  kMP_MT_invite_send,
-  kMP_MT_invite_send_close
+	kMP_MT_close = 1,
+	kMP_MT_invite,
+	kMP_MT_send_close,
+	kMP_MT_invite_close,
+	kMP_MT_invite_rank,
+	kMP_MT_invite_send,
+	kMP_MT_invite_send_close
 };
 enum class AlignText
 {
@@ -62,60 +62,71 @@ enum class PostBoxState
 	kMenu = 1
 };
 class ScrollBar;
+enum class MailFilter
+{
+	kTotal,
+	kHeart,
+	kChallenge,
+	kHelp,
+	kTicket,
+	kInvite,
+	kNews,
+	kUnknownFriendRequest
+};
 class MailPopup : public CCLayer, public CCTableViewDataSource, public CCTableViewDelegate
 {
-public:
-  static MailPopup * create (CCObject * t_close, SEL_CallFunc d_close);
-  void finishedOpen ();
-  void finishedClose ();
-  void myInit (CCObject * t_close, SEL_CallFunc d_close);
-  void loadMail ();
-  void drawMail (Json::Value obj);
-  void closePopup (CCControlButton * obj, CCControlEvent event);
-  void removeMail (CCObject * _obj);
-  virtual CCTableViewCell * tableCellAtIndex (CCTableView * table, unsigned int idx);
-  virtual void scrollViewDidScroll (CCScrollView * view);
-  virtual void scrollViewDidZoom (CCScrollView * view);
-  virtual void tableCellTouched (CCTableView * table, CCTableViewCell * cell);
-  virtual CCSize cellSizeForTable (CCTableView * table);
-  virtual unsigned int numberOfCellsInTableView (CCTableView * table);
-  void touchFriend (CCObject * a, CCControlEvent b);
-  CCPoint getContentPosition (int t_tag);
-  virtual bool ccTouchBegan (CCTouch * pTouch, CCEvent * pEvent);
-  virtual void ccTouchMoved (CCTouch * pTouch, CCEvent * pEvent);
-  virtual void ccTouchEnded (CCTouch * pTouch, CCEvent * pEvent);
-  virtual void ccTouchCancelled (CCTouch * pTouch, CCEvent * pEvent);
-  virtual void registerWithTouchDispatcher ();
-	
-	void setAlignText(AlignText at);
-	void showLeftMenuToggle(bool show);
-	void onReceiveStageSuccess();
-	void onReceiveStageFail();
-	void iHelpYou(int stage, long long user_id, const std::string& nick, Json::Value removeInfo);
-	void removeMessage(int mailNo, long long memberID, std::function<void(Json::Value)> f);
-protected:
-  Json::Value m_mailList;
-  CCTableView * mailTableView;
-  CCObject * target_close;
-  SEL_CallFunc delegate_close;
-  ScrollBar * m_scrollBar;
-	
-	std::map<AlignText, CCSprite*> m_alignTexts;
-	std::vector<CCMenuItemImageLambda*> m_menuList;
-	PostBoxState m_popupState;
-	
-	int download_card_number;
-	CCNode* loading_card_img;
-	CCNode* addCardImg(int t_card_number, int t_card_level, string t_passive);
-	void resultLoadedCardInfo (Json::Value result_data);
-	vector <DownloadFile> df_list;
-	vector <CopyFile> cf_list;
-	
-	int ing_download_cnt;
-	void successAction ();
-	void failAction();
-	void startDownloadCardImage ();
-	void startDownload ();
+	public:
+		static MailPopup * create (CCObject * t_close, SEL_CallFunc d_close);
+		void finishedOpen ();
+		void finishedClose ();
+		void myInit (CCObject * t_close, SEL_CallFunc d_close);
+		void loadMail ();
+		void drawMail (Json::Value obj);
+		void closePopup (CCControlButton * obj, CCControlEvent event);
+		void removeMail (CCObject * _obj);
+		virtual CCTableViewCell * tableCellAtIndex (CCTableView * table, unsigned int idx);
+		virtual void scrollViewDidScroll (CCScrollView * view);
+		virtual void scrollViewDidZoom (CCScrollView * view);
+		virtual void tableCellTouched (CCTableView * table, CCTableViewCell * cell);
+		virtual CCSize cellSizeForTable (CCTableView * table);
+		virtual unsigned int numberOfCellsInTableView (CCTableView * table);
+		void touchFriend (CCObject * a, CCControlEvent b);
+		CCPoint getContentPosition (int t_tag);
+		virtual bool ccTouchBegan (CCTouch * pTouch, CCEvent * pEvent);
+		virtual void ccTouchMoved (CCTouch * pTouch, CCEvent * pEvent);
+		virtual void ccTouchEnded (CCTouch * pTouch, CCEvent * pEvent);
+		virtual void ccTouchCancelled (CCTouch * pTouch, CCEvent * pEvent);
+		virtual void registerWithTouchDispatcher ();
+
+		void setAlignText(AlignText at);
+		void showLeftMenuToggle(bool show);
+		void onReceiveStageSuccess();
+		void onReceiveStageFail();
+		void iHelpYou(int stage, long long user_id, const std::string& nick, Json::Value removeInfo);
+		void removeMessage(int mailNo, long long memberID, std::function<void(Json::Value)> f);
+	protected:
+		Json::Value m_mailList; // 이 정보를 기반으로 뿌려줌. 
+		CCTableView * mailTableView;
+		CCObject * target_close;
+		SEL_CallFunc delegate_close;
+		ScrollBar * m_scrollBar;
+		MailFilter m_mailFilter;
+		std::map<AlignText, CCSprite*> m_alignTexts;
+		std::vector<CCMenuItemImageLambda*> m_menuList;
+		PostBoxState m_popupState;
+
+		int download_card_number;
+		CCNode* loading_card_img;
+		CCNode* addCardImg(int t_card_number, int t_card_level, string t_passive);
+		void resultLoadedCardInfo (Json::Value result_data);
+		vector <DownloadFile> df_list;
+		vector <CopyFile> cf_list;
+
+		int ing_download_cnt;
+		void successAction ();
+		void failAction();
+		void startDownloadCardImage ();
+		void startDownload ();
 };
 #undef LZZ_INLINE
 #endif
