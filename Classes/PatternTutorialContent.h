31b9acdbdd9a81057baff420148aad233ffda381
seo88youngho <seo88youngho@gmail.com>
2014-02-04 17:00:15 +0900

    사망시 튜토리얼, 패턴 튜토리얼, 줌 화면 디자인 수정, 퍼즐들 다시 잠굼

--- Classes/PatternTutorialContent.h
+++ Classes/PatternTutorialContent.h
@@ -52,7 +52,7 @@ private:
 		{
 			show_content->removeFromParent();
 			show_content = CCSprite::create(CCString::createWithFormat("pattern%d_tutorial.png", pattern_list[ing_close_cnt])->getCString());
-			show_content->setPosition(ccp(0, -16));
+			show_content->setPosition(ccp(0, 0));
 			addChild(show_content);
 		}
 	}
@@ -65,31 +65,36 @@ private:
 		for(int i=0;i<t_pattern_list.size();i++)
 			pattern_list.push_back(t_pattern_list[i]);
 		
-		CCScale9Sprite* case_back = CCScale9Sprite::create("popup3_case_back.png", CCRectMake(0, 0, 150, 150), CCRectMake(13, 45, 135-13, 105-13));
+		CCSprite* case_back = CCSprite::create("pattern_tutorial_back.png");
 		case_back->setPosition(CCPointZero);
 		addChild(case_back);
 		
-		case_back->setContentSize(CCSizeMake(350, 265));
+//		CCScale9Sprite* case_back = CCScale9Sprite::create("popup3_case_back.png", CCRectMake(0, 0, 150, 150), CCRectMake(13, 45, 135-13, 105-13));
+//		case_back->setPosition(CCPointZero);
+//		addChild(case_back);
+//		
+//		case_back->setContentSize(CCSizeMake(350, 265));
 		
 		ing_close_cnt = 0;
 		
 		show_content = CCSprite::create(CCString::createWithFormat("pattern%d_tutorial.png", pattern_list[ing_close_cnt])->getCString());
-		show_content->setPosition(ccp(0, -16));
+		show_content->setPosition(ccp(0, 0));
 		addChild(show_content);
 		
-		CCSprite* title_img = CCSprite::create("tutorial_popup_title.png");
-		title_img->setPosition(ccp(0, 111));
-		addChild(title_img);
+//		CCSprite* title_img = CCSprite::create("tutorial_popup_title.png");
+//		title_img->setPosition(ccp(0, 111));
+//		addChild(title_img);
 		
-		CCSprite* n_close = CCSprite::create("item_buy_popup_close.png");
-		CCSprite* s_close = CCSprite::create("item_buy_popup_close.png");
-		s_close->setColor(ccGRAY);
+		CCSprite* n_close = CCSprite::create("whitePaper.png");
+		n_close->setOpacity(0);
+		CCSprite* s_close = CCSprite::create("whitePaper.png");
+		s_close->setOpacity(0);
 		
 		CCMenuItemSprite* close_item = CCMenuItemSprite::create(n_close, s_close, this, menu_selector(PatternTutorialContent::menuAction));
 		
 		close_menu = CCMenu::createWithItem(close_item);
 		close_menu->setTouchPriority(touch_priority-1);
-		close_menu->setPosition(ccp(150,112));
+		close_menu->setPosition(ccp(0,0));
 		addChild(close_menu);
 		
 		is_menu_enable = true;
