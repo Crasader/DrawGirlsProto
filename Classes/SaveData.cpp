97a6e7c77970616a9627307f98d34a94737a18ba
hsnks100 <hsnks100@gmail.com>
2014-04-28 13:43:22 +0900

    컴파일 에러 제거.

--- Classes/SaveData.cpp
+++ Classes/SaveData.cpp
@@ -59,7 +59,7 @@ void SaveData::createJSON(string filename)
 		Json::Reader reader;
 		reader.parse("{}", file_sync[key]);
 //		.loadFromString("{}");
-		cocos2d::CCLOG("create json : %s", filename.c_str());
+		CCLOG("create json : %s", filename.c_str());
 	}
 	else
 	{
