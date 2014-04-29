88817ff85b86847c723970c0d6dcfdf3a247b347
litqoo <litqoo@gmail.com>
2014-04-30 03:46:00 +0900

    색깔공지적용

--- Classes/StarGoldData.cpp
+++ Classes/StarGoldData.cpp
@@ -175,7 +175,7 @@ string StarGoldData::getFont2() // Jrnaver
 #if CC_TARGET_PLATFORM == CC_PLATFORM_IOS
 	font_name = "jrNaver";
 #elif CC_TARGET_PLATFORM == CC_PLATFORM_ANDROID
-	font_name = "fonts/add.ttf"; //RixHeadEB.ttf //RixMGoB.ttf //RixJGoB
+	font_name = "fonts/jrNaver.ttf"; //RixHeadEB.ttf //RixMGoB.ttf //RixJGoB
 #endif
 	
 	return font_name;
