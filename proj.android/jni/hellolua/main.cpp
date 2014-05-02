17a7b9ced11c69e069a8b36fae23b735b012c81f
hsnks100 <hsnks100@gmail.com>
2014-05-02 14:34:35 +0900

    이건 왜 또 초기화됐노?

--- proj.android/jni/hellolua/main.cpp
+++ proj.android/jni/hellolua/main.cpp
@@ -30,6 +30,7 @@ void Java_org_cocos2dx_lib_Cocos2dxRenderer_nativeInit(JNIEnv*  env, jobject thi
         AppDelegate *pAppDelegate = new AppDelegate();
         CCApplication::sharedApplication()->run();
     }
+    /*
     else
     {
         ccGLInvalidateStateCache();
@@ -38,7 +39,7 @@ void Java_org_cocos2dx_lib_Cocos2dxRenderer_nativeInit(JNIEnv*  env, jobject thi
         CCTextureCache::reloadAllTextures();
         CCNotificationCenter::sharedNotificationCenter()->postNotification(EVENT_COME_TO_FOREGROUND, NULL);
         CCDirector::sharedDirector()->setGLDefaultValues(); 
-    }
+    }*/
 }
 
 }
