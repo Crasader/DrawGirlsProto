9e4281a17293af2c04b4fc29fe333d3d17552796
hsnks100 <hsnks100@gmail.com>
2014-06-03 18:50:07 +0900

    다시 되돌~

--- Classes/OptionPopup.cpp
+++ Classes/OptionPopup.cpp
@@ -172,21 +172,21 @@ bool OptionPopup::init()
 //											 1);                                 // Event Value
 	
 //	return true;
-	Json::Value param;
-	param["productid"] = "g_10289_001";
-	hspConnector::get()->purchaseProduct(param, Json::Value(), [=](Json::Value v){
-		if(v["issuccess"].asInt())
-		{
-//			reqItemDelivery(); // ;
-			Json::Value param;
-			param["memberID"] = hspConnector::get()->getMemberID();
-			GraphDog::get()->command("requestItemDelivery", param, [=](Json::Value t){
-				// 여기서 진짜 갱신.
-				KS::KSLog("refresh !!!!", t);
-			});
-		}
-		KS::KSLog("in-app test \n%", v);
-	});
+//	Json::Value param;
+//	param["productid"] = "g_10289_001";
+//	hspConnector::get()->purchaseProduct(param, Json::Value(), [=](Json::Value v){
+//		if(v["issuccess"].asInt())
+//		{
+////			reqItemDelivery(); // ;
+//			Json::Value param;
+//			param["memberID"] = hspConnector::get()->getMemberID();
+//			GraphDog::get()->command("requestItemDelivery", param, [=](Json::Value t){
+//				// 여기서 진짜 갱신.
+//				KS::KSLog("refresh !!!!", t);
+//			});
+//		}
+//		KS::KSLog("in-app test \n%", v);
+//	});
 	
 	setTouchEnabled(true);
 	
