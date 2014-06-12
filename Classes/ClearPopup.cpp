9dfca5eec04d1ee90b837224bce309084aee20b0
hsnks100 <hsnks100@gmail.com>
2014-06-12 10:53:50 +0900

    클리어 팝업 순위 변동알림. 중간 단계.

--- Classes/ClearPopup.cpp
+++ Classes/ClearPopup.cpp
@@ -897,13 +897,22 @@ void ClearPopup::resultGetTime(Json::Value result_data)
 		}
 	}
 }
-								
+
+//void ClearPopup::frontFlip()
+//{
+//	
+//}
+//void ClearPopup::backFlip()
+//{
+//	
+//}
 void ClearPopup::resultGetRank(Json::Value result_data)
 {
 	cell_action_list.clear();
 	
 	if(result_data["result"]["code"].asInt() == GDSUCCESS)
 	{
+		
 		CCSpriteFrameCache::sharedSpriteFrameCache()->addSpriteFramesWithFile("flags.plist");
 		
 		CCSprite* graph_back = CCSprite::create("ending_graph.png");
@@ -933,6 +942,9 @@ void ClearPopup::resultGetRank(Json::Value result_data)
 		int alluser = result_data["alluser"].asInt();
 		int myrank = result_data["myrank"].asInt();
 		
+		int before_stage_high_rank = myDSH->getIntegerForKey(kDSH_Key_stageHighRank_int1, mySD->getSilType()); // 이전 기록
+		
+		myDSH->setIntegerForKey(kDSH_Key_stageHighRank_int1, mySD->getSilType(), myrank);
 //		CCLabelTTF* all_user_label = CCLabelTTF::create(CCString::createWithFormat("/%d", alluser)->getCString(), mySGD->getFont().c_str(), 10);
 //		all_user_label->setColor(ccc3(255, 170, 20));
 //		all_user_label->setAnchorPoint(ccp(1,0.5));
@@ -959,15 +971,12 @@ void ClearPopup::resultGetRank(Json::Value result_data)
 		rank_percent_case->addChild(percent_label, kZ_CP_img);
 		percent_label->setOpacity(0);
 		
+		// 이까지 그래프 표시하는 부분 코드.
+		
 		
 		cell_action_list.push_back([=](){
 			CCMoveTo* t_move = CCMoveTo::create(2.f*(1.f-rank_percent), ccp(257 + 195.f*rank_percent,231));
 			rank_percent_case->runAction(t_move);
-			
-//			CCDelayTime* t_delay1 = CCDelayTime::create(1.f);
-//			CCFadeTo* t_fade1 = CCFadeTo::create(0.5f, 255);
-//			CCSequence* t_seq1 = CCSequence::create(t_delay1, t_fade1, NULL);
-//			my_rank_label->runAction(t_seq1);
 			
 			CCDelayTime* t_delay2 = CCDelayTime::create(1.f);
 			CCFadeTo* t_fade2 = CCFadeTo::create(0.5f, 255);
@@ -979,24 +988,43 @@ void ClearPopup::resultGetRank(Json::Value result_data)
 		
 		delay_index = 0;
 		int limit_count = 3;
+		auto cellSize = CCSizeMake(210, 40);
+	
+		float spinDuration = 0.3f;
+		CCScale9Sprite* t_list_cell_case_back = nullptr;
+		CCScale9Sprite* t_list_cell_case = nullptr;
 		for(int i=0;i<user_list.size() && i<limit_count;i++)
 		{
 			string case_name;
+			bool isMe = false;
 			if(myrank == i+1)
 			{
 				case_name = "mainpopup_pupple1.png";
 				limit_count++;
+				isMe = true;
 			}
 			else
 			{
 				case_name = "rank_normal.png";
 			}
+		
 			
 			CCScale9Sprite* list_cell_case = CCScale9Sprite::create(case_name.c_str(), CCRectMake(0, 0, 40, 40), CCRectMake(19, 19, 2, 2));
-			list_cell_case->setContentSize(CCSizeMake(210, 40));
-			list_cell_case->setPosition(ccp(355,197-i*36));
+			auto cellPosition = ccp(355, 197 - i*36);
+			list_cell_case->setContentSize(cellSize);
+			list_cell_case->setPosition(cellPosition);
 			main_case->addChild(list_cell_case, kZ_CP_img);
 			
+			if(isMe)
+			{
+				CCScale9Sprite* list_cell_case_back = CCScale9Sprite::create(case_name.c_str(), CCRectMake(0, 0, 40, 40), CCRectMake(19, 19, 2, 2));
+				list_cell_case_back->setContentSize(cellSize);
+				list_cell_case_back->setPosition(cellPosition);
+				t_list_cell_case_back = list_cell_case_back;
+				t_list_cell_case = list_cell_case;
+				main_case->addChild(list_cell_case_back, kZ_CP_img);
+
+			}
 			CCPoint rank_position = ccp(20,20);
 			if(i == 0)
 			{
@@ -1064,6 +1092,7 @@ void ClearPopup::resultGetRank(Json::Value result_data)
 			CCScale9Sprite* list_cell_case = CCScale9Sprite::create("mainpopup_pupple1.png", CCRectMake(0, 0, 40, 40), CCRectMake(19, 19, 2, 2));
 			list_cell_case->setContentSize(CCSizeMake(210, 40));
 			list_cell_case->setPosition(ccp(355,197-3*36));
+			t_list_cell_case = list_cell_case;
 			main_case->addChild(list_cell_case, kZ_CP_img);
 			
 			KSLabelTTF* rank_label = KSLabelTTF::create(CCString::createWithFormat("%d", myrank)->getCString(), mySGD->getFont().c_str(), 12);
@@ -1099,6 +1128,66 @@ void ClearPopup::resultGetRank(Json::Value result_data)
 				CCSequence* t_seq = CCSequence::create(t_delay, t_move, NULL);
 				list_cell_case->runAction(t_seq);
 			});
+			
+			{
+				CCScale9Sprite* list_cell_case_back = CCScale9Sprite::create("mainpopup_pupple1.png", CCRectMake(0, 0, 40, 40), CCRectMake(19, 19, 2, 2));
+
+				list_cell_case_back->setContentSize(cellSize);
+				list_cell_case_back->setPosition(original_position);
+				t_list_cell_case_back = list_cell_case_back;
+				main_case->addChild(list_cell_case_back, kZ_CP_img);
+			}
+		}
+		if(t_list_cell_case_back)
+		{
+			std::string changedRankLbl;
+			if(myrank - before_stage_high_rank < 0)
+			{
+				changedRankLbl = ccsf("▼   %d", myrank - before_stage_high_rank);
+			}
+			else
+			{
+				changedRankLbl = ccsf("▲   %d", myrank - before_stage_high_rank);
+			}
+			KSLabelTTF* changedRank = KSLabelTTF::create(changedRankLbl.c_str(), mySGD->getFont().c_str(), 12);
+			changedRank->setPosition(ccpFromSize(t_list_cell_case_back->getContentSize()) / 2.f);
+			changedRank->setColor(ccc3(255, 179, 0));
+			t_list_cell_case_back->addChild(changedRank);
+			
+			float flipDelay = 3.f;
+			frontFlip = [=](){
+				addChild(KSTimer::create(flipDelay, [=](){
+					addChild(KSGradualValue<float>::create(1.f, 0, spinDuration, [=](float t){
+						t_list_cell_case->setScaleY(t);
+					}, [=](float t){
+						t_list_cell_case->setScaleY(t);
+						
+						addChild(KSGradualValue<float>::create(0.f, 1.f, spinDuration, [=](float t){
+							t_list_cell_case_back->setScaleY(t);
+						}, [=](float t){
+							t_list_cell_case_back->setScaleY(t);
+							backFlip();
+						}));
+					}));
+				}));
+			};
+			backFlip = [=](){
+				addChild(KSTimer::create(flipDelay, [=](){
+					addChild(KSGradualValue<float>::create(1.f, 0, spinDuration, [=](float t){
+						t_list_cell_case_back->setScaleY(t);
+					}, [=](float t){
+						t_list_cell_case_back->setScaleY(t);
+						
+						addChild(KSGradualValue<float>::create(0.f, 1.f, spinDuration, [=](float t){
+							t_list_cell_case->setScaleY(t);
+						}, [=](float t){
+							t_list_cell_case->setScaleY(t);
+							frontFlip();
+						}));
+					}));
+				}));
+			};
+			frontFlip();
 		}
 		
 		is_saved_user_data = true;
