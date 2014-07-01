3740c384e46b1289a95195fee2705334b3c63a7a
hsnks100 <hsnks100@gmail.com>
2014-07-01 13:47:41 +0900

    no message

--- Classes/ASPopupView.cpp
+++ Classes/ASPopupView.cpp
@@ -71,7 +71,7 @@ ASPopupView* ASPopupView::getCommonNoti(int t_touch_priority, string t_title, CC
 	content_back->setPosition(ccp(0.0,-15.5)); 			// dt (0.0,-4.5)
 	t_container->addChild(content_back);
 	
-	KSLabelTTF* title_label = KSLabelTTF::create(t_title.c_str(), mySGD->getFont().c_str(), 16);
+	KSLabelTTF* title_label = KSLabelTTF::create(t_title.c_str(), mySGD->getFont().c_str(), 15);
 	setFormSetter(title_label);
 	title_label->setColor(ccc3(255, 170, 20));
 	title_label->setPosition(ccp(0,0));
@@ -143,6 +143,129 @@ ASPopupView* ASPopupView::getCommonNoti(int t_touch_priority, string t_title, CC
 	return t_popup;
 }
 
+ASPopupView* ASPopupView::getCommonNoti2(int t_touch_priority, string t_title, CCNode* ment_label,
+																				function<void()> close_func, CCPoint t_position, bool Xbutton)
+{
+	ASPopupView* t_popup = ASPopupView::create(t_touch_priority);
+	startFormSetter(t_popup);
+	CCSize screen_size = CCEGLView::sharedOpenGLView()->getFrameSize();
+	float screen_scale_x = screen_size.width/screen_size.height/1.5f;
+	if(screen_scale_x < 1.f)
+		screen_scale_x = 1.f;
+	
+	float height_value = 320.f;
+	if(myDSH->screen_convert_rate < 1.f)
+		height_value = 320.f/myDSH->screen_convert_rate;
+	
+	if(height_value < myDSH->ui_top)
+		height_value = myDSH->ui_top;
+	
+	t_popup->setDimmedSize(CCSizeMake(screen_scale_x*480.f, height_value));// /myDSH->screen_convert_rate));
+	
+	if(t_position.equals(CCPointZero))
+	{
+		if(mySGD->is_on_maingame)
+		{
+			t_popup->setDimmedPosition(ccp(240, myDSH->ui_center_y));
+			t_popup->setBasePosition(ccp(240, myDSH->ui_center_y));
+		}
+		else
+		{
+			t_popup->setDimmedPosition(ccp(240, 160));
+			t_popup->setBasePosition(ccp(240, 160));
+		}
+	}
+	else
+	{
+		t_popup->setDimmedPosition(t_position);
+		t_popup->setBasePosition(t_position);
+	}
+	
+	
+	CCNode* t_container = CCNode::create();
+	t_popup->setContainerNode(t_container);
+	
+	CCScale9Sprite* case_back = CCScale9Sprite::create("mainpopup_back.png", CCRectMake(0, 0, 50, 50), CCRectMake(24, 24, 2, 2));
+	setFormSetter(case_back);
+	case_back->setPosition(CCPointZero);
+	t_container->addChild(case_back);
+	
+	CCScale9Sprite* content_back = CCScale9Sprite::create("mainpopup_front.png", CCRectMake(0, 0, 50, 50), CCRectMake(24, 24, 2, 2));
+	setFormSetter(content_back);
+	content_back->setPosition(ccp(0.0,-15.5)); 			// dt (0.0,-4.5)
+	t_container->addChild(content_back);
+	
+	KSLabelTTF* title_label = KSLabelTTF::create(t_title.c_str(), mySGD->getFont().c_str(), 15);
+	setFormSetter(title_label);
+	title_label->setColor(ccc3(255, 170, 20));
+	title_label->setPosition(ccp(0,0));
+	t_container->addChild(title_label);
+	
+	setFormSetter(ment_label);
+	CCSize pSize = CCSizeMake(150, 20);
+	
+	if(ment_label->getContentSize().width>pSize.width)pSize.width = ment_label->getContentSize().width;
+	if(ment_label->getContentSize().height>pSize.height)pSize.height = ment_label->getContentSize().height;
+	ment_label->setPositionX(0);
+//	ment_label->setPosition(ccp(0,5));
+	t_container->addChild(ment_label);
+	
+	case_back->setContentSize(CCSizeMake(pSize.width+50, pSize.height + 40+80 - 40));
+	content_back->setContentSize(CCSizeMake(pSize.width+39, pSize.height+76+2.5 - 40));
+	
+	setFormSetter(case_back);
+	setFormSetter(content_back);
+	
+	if(Xbutton)
+	{
+		CommonButton* close_button = CommonButton::createCloseButton(t_popup->getTouchPriority()-5);
+		setFormSetter(close_button);
+		close_button->setPosition(ccp(case_back->getContentSize().width/2.f - 22, case_back->getContentSize().height/2.f - 22));
+		close_button->setFunction([=](CCObject* sender)
+															{
+																AudioEngine::sharedInstance()->playEffect("se_button1.mp3", false);
+																CommonAnimation::closePopup(t_popup, t_container,
+																														t_popup->getDimmedSprite(), nullptr,
+																														[=](){
+																															if(close_func)
+																																close_func();
+																															t_popup->removeFromParent();
+																														});
+																
+															});
+		t_container->addChild(close_button);
+		
+	}
+	else
+	{
+		CommonButton* close_button = CommonButton::create(myLoc->getLocalForKey(kMyLocalKey_ok), 15, CCSizeMake(70, 40), CommonButtonLightPupple, t_popup->getTouchPriority()-5);
+		setFormSetter(close_button);
+		close_button->setPosition(ccp(0,case_back->getContentSize().height/2.f*-1+16));
+		close_button->setFunction([=](CCObject* sender)
+															{
+																AudioEngine::sharedInstance()->playEffect("se_button1.mp3", false);
+																CommonAnimation::closePopup(t_popup, t_container,
+																														t_popup->getDimmedSprite(), nullptr,
+																														[=](){
+																															if(close_func)
+																																close_func();
+																															t_popup->removeFromParent();
+																														});
+															});
+		t_container->addChild(close_button);
+		
+		
+		content_back->setContentSize(content_back->getContentSize() + CCSizeMake(0, 30));
+		case_back->setContentSize(case_back->getContentSize() + CCSizeMake(0, 30));
+	}
+	
+	title_label->setPosition(ccp(0,case_back->getContentSize().height/2-22));
+	ment_label->setPositionY(content_back->getContentSize().height/2.f-40);
+	
+	t_popup->getDimmedSprite()->setOpacity(0);
+	CommonAnimation::openPopup(t_popup, t_container, t_popup->getDimmedSprite());
+	return t_popup;
+}
 
 ASPopupView* ASPopupView::getCommonNoti(int t_touch_priority, string t_title, string t_comment, function<void()> close_func, CCPoint t_position, bool XButton)
 {
