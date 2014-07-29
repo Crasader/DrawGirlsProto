a8707e31e49f1f8fb4f6f323b47308932962caa4
litqoo <litqoo@gmail.com>
2014-07-29 14:15:31 +0900

    안드로이드 컴파일에러수정

--- Classes/ASPopupView.cpp
+++ Classes/ASPopupView.cpp
@@ -19,10 +19,17 @@ ASPopupView* ASPopupView::getCommonNoti(int t_touch_priority, string t_comment)
 	return getCommonNoti(t_touch_priority, t_comment, [](){});
 }
 
+ASPopupView* ASPopupView::getCommonNoti(int t_touch_priority, const char* t_title, const char* t_comment)
+{
+	return getCommonNoti(t_touch_priority, t_title, t_comment, [](){});
+}
+
+
 ASPopupView* ASPopupView::getCommonNoti(int t_touch_priority, string t_title, string t_comment)
 {
 	return getCommonNoti(t_touch_priority, t_title, t_comment, [](){});
 }
+
 
 ASPopupView* ASPopupView::getCommonNoti(int t_touch_priority, string t_title, CCNode* ment_label,
 																				function<void()> close_func,
