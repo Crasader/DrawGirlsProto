ae1376d62e94edd6b96052c14fad8a3b0eb38be1
litqoo <litqoo@gmail.com>
2014-08-08 04:27:59 +0900

    성공?

--- proj.android/jni/hellolua/main.cpp
+++ proj.android/jni/hellolua/main.cpp
@@ -5,7 +5,6 @@
 #include <jni.h>
 #include <android/log.h>
 #include "FiveRocksCpp.h"
-#include "Kamcord-C-Interface.h"
 
 #define  LOG_TAG    "main"
 #define  LOGD(...)  __android_log_print(ANDROID_LOG_DEBUG,LOG_TAG,__VA_ARGS__)
@@ -18,9 +17,7 @@ extern "C"
 jint JNI_OnLoad(JavaVM *vm, void *reserved)
 {
     JniHelper::setJavaVM(vm);
-		KamcordAndroid_InitJVM(vm);
-
-//    fiverocks::FiveRocksBridge::setJavaVM(vm);
+    fiverocks::FiveRocksBridge::setJavaVM(vm);
     return JNI_VERSION_1_4;
 }
 
