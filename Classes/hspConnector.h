be0e3447dd73d8d5b49d6bbb279ed0b65e2cab31
litqoo <litqoo@gmail.com>
2014-08-22 14:24:52 +0900

    hspconnector 도 수정

--- Classes/hspConnector.h
+++ Classes/hspConnector.h
@@ -183,12 +183,12 @@ public:
 		GraphDog::get()->setup("drawgirls_tstore", "12345678","", pGraphdogVersion);
 	}
 	
-	bool command(const std::vector<CommandParam>& params){
-		return graphdog->command(params);
+	bool command(const std::vector<CommandParam>& params, int errorCnt=0){
+		return graphdog->command(params,errorCnt);
 	}
 	
-	bool command(const char* action, const Json::Value param,function<void(Json::Value)> func){
-		return graphdog->command(action,param,func);
+	bool command(const char* action, const Json::Value param,function<void(Json::Value)> func, int errorCnt=0){
+		return graphdog->command(action,param,func,errorCnt);
 	}
 	bool command(const char* action, const Json::Value param,CCObject* obj,function<void(Json::Value)> func){
 		return graphdog->command(action,param,obj,func);
