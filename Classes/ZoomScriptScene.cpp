62fc0ed038bed90aac13e9f508ce15f476200d30
seo88youngho <seo88youngho@gmail.com>
2014-08-25 17:06:39 +0900

    랭크업에도 회전 관련 수정

--- Classes/ZoomScriptScene.cpp
+++ Classes/ZoomScriptScene.cpp
@@ -947,11 +947,13 @@ void ZoomScript::showtimeFirstAction()
 		second_img->addChild(eye, 1, 1);
 	}
 	
-	CCFadeTo* t_fade = CCFadeTo::create(1.f/6.f, 0);
-	CCCallFunc* t_call = CCCallFunc::create(this, callfunc_selector(ZoomScript::showtimeSecondAction));
-	CCSequence* t_seq = CCSequence::createWithTwoActions(t_fade, t_call);
+//	CCFadeTo* t_fade = CCFadeTo::create(1.f/6.f, 0);
+//	CCCallFunc* t_call = CCCallFunc::create(this, callfunc_selector(ZoomScript::showtimeSecondAction));
+//	CCSequence* t_seq = CCSequence::createWithTwoActions(t_fade, t_call);
+//	
+//	white_paper->runAction(t_seq);
 	
-	white_paper->runAction(t_seq);
+	showtimeSecondAction();
 }
 
 void ZoomScript::showtimeFifthAction()
@@ -997,6 +999,10 @@ void ZoomScript::showtimeFifthAction()
 	
 	game_node->setScale(1.5f);
 	game_node->setPosition(ccp(240,myDSH->ui_center_y));//ccp(0,-430*game_node->getScale()+480*screen_size.height/screen_size.width));
+	
+	game_node->setRotation(0);
+	card_back->setRotation(0);
+	
 	if(second_img)
 		second_img->removeFromParent();
 	else
