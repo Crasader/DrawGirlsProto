8814cd1a113b08eab6512b1a2728a5c8e3fd2417
litqoo <litqoo@gmail.com>
2014-08-28 15:17:25 +0900

    안드로이드 버전업

--- proj.android/src/com/nhnent/SKSUMRAN/DGproto.java
+++ proj.android/src/com/nhnent/SKSUMRAN/DGproto.java
@@ -81,7 +81,7 @@ public class DGproto extends KSActivityBase{//Cocos2dxActivity{
 		FiveRocks.setGLSurfaceView(Cocos2dxGLSurfaceView.getInstance());
 		//FiveRocks.setDebugEnabled(true);
 
-		if(com.litqoo.lib.hspConnector.setup(10331, "SKSUMRAN", "1.0.0.KG")){
+		if(com.litqoo.lib.hspConnector.setup(10331, "SKSUMRAN", "1.0.1")){
 			Log.i("com.litqoo.dgproto", "hspcore create ok");
 			com.litqoo.lib.hspConnector.testRegisterListener();
 		}else{
