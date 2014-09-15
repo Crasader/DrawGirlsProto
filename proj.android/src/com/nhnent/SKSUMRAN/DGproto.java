c2146e33b82e6b1ac9b8293d29486d3df04a0993
litqoo <litqoo@gmail.com>
2014-09-15 17:34:35 +0900

    안드로이드 소프트키 화면 짤리는 문제

--- proj.android/src/com/nhnent/SKSUMRAN/DGproto.java
+++ proj.android/src/com/nhnent/SKSUMRAN/DGproto.java
@@ -32,6 +32,7 @@ import org.json.JSONObject;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.PackageManager.NameNotFoundException;
@@ -47,7 +48,6 @@ import com.hangame.hsp.HSPCore;
 import com.hangame.hsp.HSPOAuthProvider;
 import com.hangame.hsp.HSPResult;
 import com.hangame.hsp.HSPState;
-
 import com.kamcord.android.Kamcord;
 import com.igaworks.IgawCommon;
 //import com.kamcord.android.Kamcord;
@@ -129,9 +129,9 @@ public class DGproto extends KSActivityBase{//Cocos2dxActivity{
 		  if (Build.VERSION.SDK_INT >= 11){
 			  int hideOption = Cocos2dxGLSurfaceView.SYSTEM_UI_FLAG_LAYOUT_STABLE
 	                  | Cocos2dxGLSurfaceView.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-	                  | Cocos2dxGLSurfaceView.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-	                  | Cocos2dxGLSurfaceView.SYSTEM_UI_FLAG_HIDE_NAVIGATION // hide nav bar
-	                  | Cocos2dxGLSurfaceView.SYSTEM_UI_FLAG_FULLSCREEN; // hide status bar
+	                //  | Cocos2dxGLSurfaceView.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+	                  | Cocos2dxGLSurfaceView.SYSTEM_UI_FLAG_HIDE_NAVIGATION; // hide nav bar
+	                //  | Cocos2dxGLSurfaceView.SYSTEM_UI_FLAG_FULLSCREEN; // hide status bar
 			  
 			  if (Build.VERSION.SDK_INT >= 19){
 				  hideOption = hideOption | Cocos2dxGLSurfaceView.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
@@ -273,9 +273,11 @@ public class DGproto extends KSActivityBase{//Cocos2dxActivity{
         super.onDestroy();
         android.os.Process.killProcess(android.os.Process.myPid());
     }
-    //    protected void onActivityResult(int requestCode, int resultCode, Intent data){
-//    	com.litqoo.lib.hspConnector.onActivityResult(requestCode, resultCode, data, this);
-//    }
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+    	super.onActivityResult(requestCode, resultCode, data);
+    	hideSystemUI();
+    	//com.litqoo.lib.hspConnector.onActivityResult(requestCode, resultCode, data, this);
+    }
     @Override
     protected void onStart() {
       super.onStart();
