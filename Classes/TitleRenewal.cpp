902eb78b4dee1aee1f1223910aadf3a5ca33325e
litqoo <litqoo@gmail.com>
2014-10-14 04:18:47 +0900

    골드밸런싱 기본설정가능하도록

--- Classes/TitleRenewal.cpp
+++ Classes/TitleRenewal.cpp
@@ -1216,6 +1216,7 @@ void TitleRenewalScene::resultGetCommonSetting(Json::Value result_data)
 		mySGD->setDiaryDownUrl(result_data["appDownUrls"]["diary"].asString());
 		mySGD->setIosMenuVisible(result_data["iosMenuVisible"].asInt());
 		mySGD->setIosHideVer(result_data["iosHideVer"].asString());
+		mySGD->setGoldBalance(result_data["goldBalance"].asString());
 	}
 	else
 	{
