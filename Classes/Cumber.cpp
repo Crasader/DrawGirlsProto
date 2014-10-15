89550c07222d5232996ee141abf57dc884bdecf9
litqoo <litqoo@gmail.com>
2014-10-15 22:59:22 +0900

    벨런싱수정

--- Classes/Cumber.cpp
+++ Classes/Cumber.cpp
@@ -540,6 +540,7 @@ void CumberParent::myInit()
 		mainCumber->assignBossData(root[i]);
 		mainCumber->applyPassiveData(mySD->getPassiveData());
 		mainCumber->settingAttackPercent(boss["attackpercent"].asDouble());
+		mainCumber->settingPattern(boss["pattern"]);
 		mainCumber->applyAutoBalance();
 		//	mainCumber->settingPattern(boss["pattern"]);
 		//	mainCumber->settingPattern("{\"test\":123");
@@ -555,7 +556,6 @@ void CumberParent::myInit()
 		//										{"pattern":"105", "atype":"special", "percent":1}]           )", temp_root);
 
 		//	temp_reader.parse(boss["pattern"])
-		mainCumber->settingPattern(boss["pattern"]);
 
 		//	);
 		IntPoint mapPoint;
