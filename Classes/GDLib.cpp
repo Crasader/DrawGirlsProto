eb97573f515eec147ccb1aa046fa8a6b8deee9cd
hsnks100 <hsnks100@gmail.com>
2014-11-06 02:22:50 +0900

    graphdoglib::trim

--- Classes/GDLib.cpp
+++ Classes/GDLib.cpp
@@ -48,8 +48,10 @@ namespace GraphDogLib {
 	}
 	
 	std::string trim(std::string str){
-		if(str == "")return str;
-		std::remove(str.begin(), str.end(), ' ');
+		string::size_type pos1 = str.find_first_not_of(' ');
+		string::size_type pos2 = str.find_last_not_of(' ');
+		str = str.substr(pos1 == string::npos ? 0 : pos1,
+		pos2 == string::npos ? str.length() - 1 : pos2 - pos1 + 1);
 		return str;
 	}
 	
