73064a01e4100840aa8c500c9a3d6ff1cb04d158
hsnks100 <hsnks100@gmail.com>
2014-12-12 15:15:07 +0900

    보스 레이드 팝업.

---
+++ Classes/BossRaidListPopup.h
@@ -0,0 +1,14 @@
+//
+//  BossRaidListPopup.h
+//  DGproto
+//
+//  Created by ksoo k on 2014. 12. 12..
+//
+//
+
+#ifndef __DGproto__BossRaidListPopup__
+#define __DGproto__BossRaidListPopup__
+
+#include <stdio.h>
+
+#endif /* defined(__DGproto__BossRaidListPopup__) */
