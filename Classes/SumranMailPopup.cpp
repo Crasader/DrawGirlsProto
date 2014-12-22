a10ce9f024de9d9890950bf2ea92296f6f41817b
seo88youngho <seo88youngho@gmail.com>
2014-12-22 18:44:50 +0900

    캐릭터 선물 받으면 바로 갱신되도록

--- Classes/SumranMailPopup.cpp
+++ Classes/SumranMailPopup.cpp
@@ -1116,6 +1116,7 @@ void SumranMailPopup::rewardDown(Json::Value reward, std::function<void(bool)> f
 }
 
 void SumranMailPopup::takedCheck(Json::Value reward, std::function<void(void)> func){
+	
 	if(reward.isArray()){
 		
 		
@@ -1123,6 +1124,10 @@ void SumranMailPopup::takedCheck(Json::Value reward, std::function<void(void)> f
 			if(reward[i].get("type","box").asString()=="cd"){
 				takedCard(reward[i]["count"].asInt(),func);
 				return;
+			}
+			else if(reward[i].get("type", "box").asString() == "cp")
+			{
+				mySGD->refreshCharacterHistory(reward[i]["result"]);
 			}
 		}
 	}
